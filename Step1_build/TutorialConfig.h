edeb0ff86083b57cd3f2fafbbd21007ea74976f9
lsk <enclyde@tongji.edu.cn>
2025-01-18 15:54:29 +0800

    first commit

---
+++ Step1_build/TutorialConfig.h
@@ -0,0 +1,19 @@
+// the configured options and settings for Tutorial
+// TODO 10: Define Tutorial_VERSION_MAJOR and Tutorial_VERSION_MINOR
+
+// the configured options and settings for Tutorial
+#define Tutorial_VERSION_MAJOR 1
+#define Tutorial_VERSION_MINOR 0
+
+#ifndef TutorialConfig_h
+class A{
+    public:
+    // A();
+    // ~A();
+    void print();
+};
+
+void A::print(){
+    std::cout << "12333";
+}
+#endif
